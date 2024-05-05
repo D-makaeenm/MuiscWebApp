e0c198e5ca878eacc377c8e809044114fccb3ba6
D-makaeenm <bachduyatk300@gmail.com>
2024-05-05 17:19:21 +0700

    Your commit message here

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Trang Mới</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.4/jquery-confirm.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.4/jquery-confirm.min.js"></script>
+    <link rel="stylesheet" href="index.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div id="left-bar">
+                <button id="logo">
+                    <img id="logo" src="image\logo.png" alt="logo.png">
+                </button>
+            </div>
+            <div class="col">
+                <div id="nav-bar">
+                    <input id="search" type="text" class="form-control" placeholder="Tìm kiếm">
+                    <button id="login" class="btn" >Đăng nhập</button>
+                </div>
+                <div style="color:#fff">yyy</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
